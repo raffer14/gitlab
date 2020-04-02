680d24c9fef039065ef993b6b803ddd57953a6a8
raffer14 <63068779+raffer14@users.noreply.github.com>
2020-04-02 14:16:51 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+ public static void main( String argv[] )
+ {
+ String name = "World";
+ if ( argv.length != 0 )
+ {
+ name = argv[0];
+ }
+ System.out.println( "Hello, " + name + "!" );
+ }
+}
