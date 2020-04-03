191946f74bb9a3b7dc532786a0929e2cc11b5c2c
raffer14 <63068779+raffer14@users.noreply.github.com>
2020-04-02 20:42:17 -0400

    Create Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+ public static void main( String argv[] )
+ {
+	 //
+ String name = "World";
+ if ( argv.length != 0 )
+ {
+ name = argv[0];
+ }
+ System.out.println( "Hello, " + name + "!" );
+ }
+}
